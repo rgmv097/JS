8dcc445676081031d6471884c4575122536f09b0
rgmv097 <chingiznr@code.edu.az>
2022-07-01 20:29:57 +0400

    HW

--- Task-2/assets/js/app.js
+++ Task-2/assets/js/app.js
@@ -1,8 +1,8 @@
 //Daire sahesi
 var r=5;
-alert("Dairenin Sahesi\n"+Math.floor(Math.PI*(r*r)));
+alert("Dairenin Sahesi\n"+Math.floor(Math.PI*Math.pow(r, 2)));
 //kvadrat sahesi
 var a=5;
-alert("Kvadratin Sahesi\n"+Math.floor(a*a));
+alert("Kvadratin Sahesi\n"+Math.floor(Math.pow(a, 2)));
 
 
